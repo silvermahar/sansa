10932ddafe626d3d0657392b72b33a78e9369b99
Silver <silver.mahar@khk.ee>
2012-12-15 14:13:45 +0200

    Renamed Welcome controller to Dashboard. Added form for tweeting

--- application/classes/Controller/Dashboard.php
+++ application/classes/Controller/Dashboard.php
@@ -1,11 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Dashboard extends Controller_Template {
+class Controller_Dashboard extends Controller_Template
+{
 
-	public function action_index()
-	{
+    public function action_index()
+    {
         $tweet = $this->request->post('tweet');
 
-	}
+    }
 
 } // End Welcome
