208271d049b507261120a33bc4efa502e37c8acc
Silver <silver.mahar@khk.ee>
2012-12-15 13:31:28 +0200

    Enabled commoneer module. Added bootstrap template. Added Welcome view

---
+++ application/logs/2012/12/15.php
@@ -0,0 +1,90 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2012-12-15 04:51:59 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 04:51:59 --- DEBUG: #0 /var/www/sansa/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/sansa/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/sansa/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/sansa/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/sansa/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/sansa/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/sansa/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/sansa/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 04:54:43 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 04:54:43 --- DEBUG: #0 /var/www/sansa/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/sansa/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/sansa/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/sansa/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/sansa/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/sansa/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/sansa/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/sansa/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 04:54:44 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 04:54:44 --- DEBUG: #0 /var/www/sansa/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/sansa/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/sansa/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/sansa/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/sansa/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/sansa/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/sansa/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/sansa/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:20 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:20 --- DEBUG: #0 /var/www/sansa/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/sansa/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/sansa/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/sansa/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/sansa/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/sansa/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/sansa/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/sansa/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:21 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:21 --- DEBUG: #0 /var/www/sansa/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/sansa/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/sansa/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/sansa/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/sansa/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/sansa/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/sansa/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/sansa/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:21 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:21 --- DEBUG: #0 /var/www/sansa/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/sansa/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/sansa/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/sansa/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/sansa/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/sansa/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/sansa/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/sansa/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:21 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:21 --- DEBUG: #0 /var/www/sansa/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/sansa/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/sansa/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/sansa/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/sansa/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/sansa/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/sansa/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/sansa/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:23 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/sansa/system/classes/Kohana/View.php:137
+2012-12-15 05:00:23 --- DEBUG: #0 /var/www/sansa/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/sansa/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/sansa/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/sansa/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/sansa/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/sansa/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/sansa/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/sansa/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/sansa/system/classes/Kohana/View.php:137
